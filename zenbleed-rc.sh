9708cc0144307c798cf257716a221fa0fe18a777
quadhelion.engineering <erogravity@fastmail.com>
2023-08-12 13:06:59 -0700

    Removed duplicate load_rc_config

--- zenbleed-rc.sh
+++ zenbleed-rc.sh
@@ -16,15 +16,12 @@ rcvar="${name}_enable"
 start_cmd="msr_set"
 
 
-load_rc_config "${name}"
-
 : "${zenbleed_enable:=NO}"
-
 
 msr_set() {
     cores=$(ls -l /dev | grep "cpuctl" | wc -l)
     a=0; while [ $a -lt $cores ]; do cpucontrol -m '0xc0011029|=0x200' /dev/cpuctl$a; a=$((a + 1)); done
 }
 
-load_rc_config $name
+load_rc_config "${name}"
 run_rc_command "$1" 
